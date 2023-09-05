ad5d0fac7db13e91257019b460740014664749c0
Adam Khodier <khodieradam1297@gmail.com>
2023-09-04 21:33:08 -0400

    defining each line of codes Big O

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function funChallenge(input) {
-    let a = 10
-    a = 50 + 3
+  let a = 10; //O(1)
+  a = 50 + 3; //O(1)
 
-    for (let i = 0; i < input.length; i++) {
-        anotherFunction()
-        let stranger = true
-        a++
-    }
-    return a
+  for (let i = 0; i < input.length; i++) { //O(n) n is the input
+    anotherFunction(); //O(n) runs depending on how big input is
+    let stranger = true; //O(n) runs depending on how big input is
+    a++; //O(n) runs depending on how big input is
+  }
+  return a; //O(1)
 }
+
+// BIG O(3 + 4n) eventually simplified to just O(n)
+
+funChallenge();
