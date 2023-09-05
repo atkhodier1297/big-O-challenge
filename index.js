f110ebe1308e6abbad36da4a24f625560430bd92
Adam Khodier <khodieradam1297@gmail.com>
2023-09-05 11:41:41 -0400

    Quad time and nested Big O solving

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ function funChallenge(input) {
 
 // BIG O(3 + 4n) eventually simplified to just O(n)
 
-
-
 function anotherFunChallenge(input) {
   let a = 5; //O(1) assigning variables
   let b = 10; //O(1)
@@ -32,3 +30,52 @@ function anotherFunChallenge(input) {
 }
 
 // BIG O(4 + 5n) eventually simplified to just O(n)
+
+// In interviews you will never have to calc the exact Big O but rather simplify them.
+// Rules to Follow
+
+// We drop the constants and only care about n if it's growing.
+// O(n / 2 + 100) === O(n + 1) you can even drop the 1 and just leave it as O(n)
+// if you had two separate loops making them both O(n) then it would be O(2n) simplified as O(n)
+// we care more about how the line moves linearly as opposed to how steep it is
+// ALWAYS DROP THE CONSTANTS
+
+// Drop the dominants
+function compressBoxTwice(boxes, boxesTwo) {
+  boxes.forEach(function(boxes){
+    console.log(boxes)
+  })
+  // depends on first item
+
+  boxesTwo.forEach(function(boxes){
+    console.log(boxes)
+  })
+  // depends on second item
+}
+
+// Different rules for different inputs boxes and boxesTwo are not the same
+// Therefore O(n) is not the Big O of the above function
+// The Big O is actually O(a + b) they are not looping over same items
+
+//What to do in a nested loop situation
+
+// Log all pairs of array
+const boxes = ["a", "b", "c", "d", "e"]
+
+function logAllPairs(array){
+  for (let i = 0; i < array.length; i++){
+    for (let j = 0; j < array.length; j++){
+      console.log(array[i], array[j])
+    }
+  }
+}
+
+logAllPairs(boxes)
+
+// What is the Big O of something like this that has nested loops
+// O(n * n) === O(n^2) Quadratic Time
+// Line bends up and increases as the number of elements increases number of operations increases SIGNIFICANTLY
+// Very complex compared to Linear Time and Constant Time
+
+// Operations that are not nested is addition operations that are nested is multiplication
+// If there was multiple arguments and nested loops then it would be O(a * b)
